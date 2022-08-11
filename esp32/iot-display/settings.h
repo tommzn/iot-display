2985ca5c5551cb593f0959a10955d036637b0123
Thomas Schenker <tommzn@gmx.de>
2022-08-11 11:49:52 +0200

    refactor settings for connect attemps; add default deep sleep settings

--- esp32/iot-display/settings.h
+++ esp32/iot-display/settings.h
@@ -1,12 +1,15 @@
 // Factors to convert seconds to milli and nicro settings
-#define uS_TO_S_FACTOR 1000000
+#define uS_TO_S_FACTOR 1000000ULL
 #define mS_TO_S_FACTOR 1000
 
+// Default sleep time, 10 min
+#define SECONDS_TO_SLEEP 600
+
 // Max Wifi connect attemps
-#define MAX_WIFI_CONNECT_ATTEMPS 10
+#define WIFI_MAX_CONNECT_ATTEMPS 10
 
 // Max AWS IOT connect attemps
-#define MAX_AWS_IOT_CONNECT_ATTEMPS 10
+#define AWS_IOT_MAX_CONNECT_ATTEMPS 10
 
 // AWS IOT settings
 const char AWS_IOT_THING_NAME[] = "<AWS IOT Thing Name>";
