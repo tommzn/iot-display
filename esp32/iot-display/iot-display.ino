15ad76dce56b24b305bb8924f1765fe3e73b3a86
Thomas Schenker <tommzn@gmx.de>
2022-08-11 11:47:14 +0200

    add ESP32 deep sleep

--- esp32/iot-display/iot-display.ino
+++ esp32/iot-display/iot-display.ino
@@ -15,21 +15,40 @@
 // Include WiFi credentials
 #include "wifi_credentials.h"
 
-// Include settings for Wifi and AWs IOT.
+// Include settings for Wifi and AWS IOT
 #include "settings.h"
 
 // WiFi connection handler.
 #include "WiFiConnection.h"
-static WiFiConnection wifi = WiFiConnection(WIFI_SSID, WIFI_PASSWORD, MAX_WIFI_CONNECT_ATTEMPS);
+static WiFiConnection wifi = WiFiConnection(WIFI_SSID, WIFI_PASSWORD, WIFI_MAX_CONNECT_ATTEMPS);
 
+// Will start deep sleep for defined number of seconds
+void enterDeepSleep() {
+
+  delay(1000);
+  
+  Serial.println("Setup ESP32 deep sleep for " + String(SECONDS_TO_SLEEP) + " seconds.");
+  esp_sleep_enable_timer_wakeup(SECONDS_TO_SLEEP * uS_TO_S_FACTOR);
+
+  Serial.println("Going to deep sleep now!");
+  Serial.flush(); 
+  esp_deep_sleep_start();
+}
 
 void setup() {
 
+  // Try to connect to WiFi with given settings and credentials
   wifi.connect();
 }
 
 void loop() {
 
-  delay(5000);
+  delay(500);
+
+  // Disconnect from WiFi
   wifi.disconnect();
+
+  // Going to deep sleep for defined number of seconds
+  // See SECONDS_TO_SLEEP in settings.h for default value
+  enterDeepSleep();
 }
