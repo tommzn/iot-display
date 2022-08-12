9a7bba415e15406df440a0f28900c409ce21ba30
Thomas Schenker <tommzn@gmx.de>
2022-08-12 23:28:47 +0200

    Add device shadow topics

--- esp32/iot-display/settings.h
+++ esp32/iot-display/settings.h
@@ -10,6 +10,9 @@
 // Default sleep time, 10 min
 #define SECONDS_TO_SLEEP 600
 
+// Number of seconds the device listens for content updates
+#define MAX_RUNTIME_SECONDS 30
+
 // Max Wifi connect attemps
 #define WIFI_MAX_CONNECT_ATTEMPS 10
 
@@ -17,9 +20,18 @@
 #define AWS_IOT_MAX_CONNECT_ATTEMPS 10
 
 // AWS IOT settings
-const char AWS_IOT_THING_NAME[] = "<AWS IOT Thing Name>";
-const char AWS_IOT_ENDPOINT[]   = "<your endpoint>-ats.iot.<region>.amazonaws.com";
-const char AWS_IOT_LOG_TOPIC[]  = "iot-display/logs";
+const char AWS_IOT_THING_NAME[]    = "<AWS IOT Thing Name>";
+const char AWS_IOT_ENDPOINT[]      = "<your endpoint>-ats.iot.<region>.amazonaws.com";
+const char AWS_IOT_LOG_TOPIC[]     = "logs";
+const char AWS_IOT_CONTENT_TOPIC[] = "contents";
+
+// Topics to interact with AWS IOT device shadow
+const char AWS_IOT_SHADOW_GET_TOPIC[]           = "$aws/things/shadow-test/shadow/name/settings/get";
+const char AWS_IOT_SHADOW_GET_ACCEPTED_TOPIC[]  = "$aws/things/shadow-test/shadow/name/settings/get/accepted";
+const char AWS_IOT_SHADOW_UPDATE_TOPIC[]        = "$aws/things/<AWS IOT Thing Name>/shadow/name/settings/update";
+const char AWS_IOT_SHADOW_REJECT_GET_TOPIC[]    = "$aws/things/shadow-test/shadow/name/settings/get/rejected";
+const char AWS_IOT_SHADOW_REJECT_UPDATE_TOPIC[] = "$aws/things/shadow-test/shadow/name/settings/update/rejected";
+
 
 #endif
   
