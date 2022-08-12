0721b5e722c2c65ba0d5bc64072ba09d0886ce37
Thomas Schenker <tommzn@gmx.de>
2022-08-12 23:27:38 +0200

    add message handler and update docs

--- esp32/iot-display/iot-display.ino
+++ esp32/iot-display/iot-display.ino
@@ -10,27 +10,49 @@
  */
 #include <Preferences.h>
 
-// Include WiFi credentials
+// Include WiFi credentials.
 #include "wifi_credentials.h"
 
-// Include settings for Wifi and AWS IOT
+// Include settings for Wifi and AWS IOT.
 #include "settings.h"
 
 // Include persistence mananger for settings.
 #include "nvs.h"
 
-// Include AWS IOT client.
+// Include and create AWS IOT client.
 #include "aws_iot_client.h"
 AwsIotClient aws_iot_client;
 
-// WiFi connection handler.
+// Include and create WiFi connection handler.
 #include "WiFiConnection.h"
 static WiFiConnection wifi = WiFiConnection(WIFI_SSID, WIFI_PASSWORD, WIFI_MAX_CONNECT_ATTEMPS);
 
 // Create new settings manager.
 Settings settings(new Preferences(), SECONDS_TO_SLEEP);
 
-// Will start deep sleep for defined number of seconds
+// Runtime timer to restrict time this device it active and waiting ofr incoming messages.
+#include "timer.h"
+Timer runtimeTimer;
+
+// Handler for incoming messages from AWS IOT.
+void handleAwsIotMessage(String &topic, String &payload) {
+
+  if (topic.endsWith("rejected")) {
+    Serial.println("Reject received, topic: " + topic);
+    Serial.println("Reason: " + payload);
+    return;
+  } 
+
+  if (topic.endsWith("get/accepted")) {
+    Serial.println("Shadow data received, topic: " + topic);
+    Serial.println("Content: " + payload);
+    return;
+  } 
+  Serial.println("incoming: " + topic + " - " + payload);
+}
+
+// Will start deep sleep for defined number of seconds.
+// Default sleep time is defined by SECONDS_TO_SLEEP.
 void enterDeepSleep() {
 
   delay(1000);
@@ -56,29 +78,48 @@ void setup() {
   // Try to connect to WiFi with given settings and credentials
   wifi.connect();
 
+  // After WiFi connectin is establishes...
   if (wifi.connected()) {
+
+      // ...try to connect to AWS IOT.
       if (aws_iot_client.connect()) {
         Serial.println("Connected to AWS IOT!");
+        // Assign handler for incoming messages.
+        aws_iot_client.handleMessage(handleAwsIotMessage);
+        aws_iot_client.publishLogMessage("Subscribed to content topic. Waiting for updates.");
       } else {
         Serial.println("Unable to connect to AWS IOT!");
       }
   }
+
+  runtimeTimer.start(MAX_RUNTIME_SECONDS);
 }
 
 void loop() {
 
   delay(500);
-
-  // Disconnect from AWS IOT
-  aws_iot_client.disconnect();
+  // Send and receive messages.
+  aws_iot_client.loop();
   
-  // Disconnect from WiFi
-  wifi.disconnect();
+  // If runtime exceeds, shut down.
+  if (runtimeTimer.isExpired()) {
 
-  // Close settings manager
-  settings.end();
-
-  // Going to deep sleep for defined number of seconds
-  // See SECONDS_TO_SLEEP in settings.h for default value
-  enterDeepSleep();
+    String logMessage = "Stop listening and going to deep sleep for " + String(settings.getSleepTime()) + " seconds.";
+    aws_iot_client.publishLogMessage(logMessage.c_str());
+    
+    // @ToDo: Publish current settings to device shadow.
+    
+    // Disconnect from AWS IOT
+    aws_iot_client.disconnect();
+    
+    // Disconnect from WiFi
+    wifi.disconnect();
+  
+    // Close settings manager
+    settings.end();
+  
+    // Going to deep sleep for defined number of seconds
+    // See SECONDS_TO_SLEEP in settings.h for default value
+    enterDeepSleep();
+  }
 }
