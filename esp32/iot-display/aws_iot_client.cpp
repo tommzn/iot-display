62269eb62dc3e1dc2b2bc4bd13c97d2a00eb5a6f
Thomas Schenker <tommzn@gmx.de>
2022-08-12 23:26:29 +0200

    Add message handler and device shadow topics

--- esp32/iot-display/aws_iot_client.cpp
+++ esp32/iot-display/aws_iot_client.cpp
@@ -1,8 +1,13 @@
 #include "aws_iot_client.h"
+#include "timer.h"
 
 // Include AWS IOT devices certificates
 #include "aws_iot_certs.h"
 
+// Connects to AWS IOT endpoint specified by AWS_IOT_ENDPOINT using thing name from AWS_IOT_THING_NAME.
+// Since it's a encryted connection certs from AWS_CERT_CA, AWS_CERT_PRIVATE and AWS_CERT_CRT are used.
+// After successful connect client subsribes to content topic AWS_IOT_CONTENT_TOPIC and 
+// shadow topics AWS_IOT_SHADOW_GET_ACCEPTED_TOPIC, AWS_IOT_SHADOW_REJECT_GET_TOPIC and AWS_IOT_SHADOW_REJECT_UPDATE_TOPIC.
 bool AwsIotClient::connect() {
   
   // Configure WiFiClientSecure to use the AWS IoT device credentials
@@ -18,19 +23,29 @@ bool AwsIotClient::connect() {
 
   m_iot_client.begin(m_iot_endpoint, 8883, m_secure_client);
   m_iot_client.setKeepAlive(m_connection_keep_alive); 
-  
+
+  if (m_iot_client.connected()) {
+    m_iot_client.subscribe(m_content_topic);
+    m_iot_client.subscribe(m_shadow_get_accepted_topic);
+    m_iot_client.subscribe(m_shadow_reject_get_topic);
+    m_iot_client.subscribe(m_shadow_reject_update_topic);
+  }  
   return m_iot_client.connected();
 }
 
+// Disconnects from AWS IOT with some delay defined by m_disconnect_delay.
+// During this delay MQTT will flush remaining messages.
 bool AwsIotClient::disconnect() {
-  
-  unsigned long endAt = millis() + m_disconnect_delay;
-  while (endAt > millis()) {
-    m_iot_client.loop();
+
+  Timer timer;
+  timer.start(m_disconnect_delay);
+  while (!timer.isExpired()) {
+    loop();
   }
   m_iot_client.disconnect();
 }
 
-void AwsIotClient::publishLog(const char* message) {
+// Send given message to log topic defined by AWS_IOT_LOG_TOPIC.
+void AwsIotClient::publishLogMessage(const char* message) {
   m_iot_client.publish(m_log_topic, message);
 }
--- esp32/iot-display/aws_iot_client.h
+++ esp32/iot-display/aws_iot_client.h
@@ -19,7 +19,18 @@ public:
 
   // Writes given message to defined log topic.
   // See AWS_IOT_LOG_TOPIC in settings.h
-  void publishLog(const char* message);
+  void publishLogMessage(const char* message);
+
+  // Handler for incoming messages.
+  void handleMessage(MQTTClientCallbackSimpleFunction cb) {
+    m_iot_client.onMessage(cb);
+    triggerShadowGet();
+  };
+
+  // Send and receive messages.
+  void loop() {
+    m_iot_client.loop();
+  }
   
 private:
 
@@ -41,11 +52,26 @@ private:
   // Topic log messages will be send to.
   const char* m_log_topic = AWS_IOT_LOG_TOPIC;
 
+  // Topic this device subsribes to, to get new content for display updates.
+  const char* m_content_topic = AWS_IOT_CONTENT_TOPIC;
+
+  // AWS IOT shadow topics.
+  const char* m_shadow_get_topic           = AWS_IOT_SHADOW_GET_TOPIC;
+  const char* m_shadow_get_accepted_topic  = AWS_IOT_SHADOW_GET_ACCEPTED_TOPIC;
+  const char* m_shadow_update_topic        = AWS_IOT_SHADOW_UPDATE_TOPIC;
+  const char* m_shadow_reject_get_topic    = AWS_IOT_SHADOW_REJECT_GET_TOPIC;
+  const char* m_shadow_reject_update_topic = AWS_IOT_SHADOW_REJECT_UPDATE_TOPIC;
+
   // Keep connection alive for given seconds, in case send/retrieve data take a little bit longer.
   uint32_t m_connection_keep_alive = 60;
 
   // Delay befreo "real" disconnect from AWS IOT to process all MQTT messages.
   uint32_t m_disconnect_delay = 1000;
+  
+  // Trigger GET topic for shadow device to initiate distribution of current shadow state.
+  void triggerShadowGet() {
+    m_iot_client.publish(m_shadow_get_topic, "");
+  };
   
 };
 #endif
